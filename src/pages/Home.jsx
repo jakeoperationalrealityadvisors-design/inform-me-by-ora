4be25df68a4f5bd56186ac333f5b497c484a459b
Jake Bernard <jake.operationalrealityadvisors@gmail.com>
2025-12-30 23:56:41 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -72,7 +72,7 @@ export default function Home() {
                         <div className="flex gap-2">
                             <Link to={createPageUrl('Documents')}>
                                 <Button variant="ghost" size="icon" className="rounded-full hover:bg-slate-100 text-slate-600">
-                                    <FileText className="w-5 h-5" />
+                                    <FolderOpen className="w-5 h-5" />
                                 </Button>
                             </Link>
                             <Link to={createPageUrl('DailyTasks')}>
